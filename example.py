dd59079538f211123d832da6ecd83e7622d077e9
German Chaverri Campos <germanch90@gmail.com>
2022-07-27 10:21:09 -0600

    Add an example file

---
+++ example.py
@@ -0,0 +1,34 @@
+from FunctionCollection import FunctionCollection
+from LinearFunction import LinearFunction
+
+if __name__ == '__main__':
+    oferta = LinearFunction(
+        name="Oferta",
+        point1=(2000, 250),
+        point2=(4000, 750)
+    )
+    demanda = LinearFunction(
+        name="Demanda",
+        point1=(2500, 450),
+        point2=(3000, 300)
+    )
+
+    FunctionCollection(
+        oferta,
+        demanda,
+        LinearFunction(
+            name="Otra",
+            point1=(3500, 450),
+            point2=(6000, 300)
+        )
+    ).graph_all(step=20, samples=200)
+
+    print(demanda.compute_x(250)*250)
+
+    print(demanda.compute_y(3000*1.2))
+
+    print(oferta.compute_x(0))
+
+    print(oferta.equilibrium_point(demanda))
+
+
